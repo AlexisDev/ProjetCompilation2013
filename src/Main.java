c4f9897cafa2414ce651070e69f006ba808e0e07
AlexisDev <alexisgirarddev@gmail.com>
2013-12-05 01:23:34 -0800

    Build ok

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main {
         public static void main(String[] args) {
                 try {
                         FileReader  myFile = new FileReader(args[0]);
-                        CalculetteLexer myLex = new CalculetteLexer(myFile);
-                        CalculetteParser myParser = new CalculetteParser(myLex);
+                        CompilateurLexer myLex = new CompilateurLexer(myFile);
+                        CompilateurParser myParser = new CompilateurParser(myLex);
                         try {
                                 myParser.parse();
                         }

0df34cedbc2e69ecb6838d6de4c8ea642e844299
AlexisDev <alexisgirarddev@gmail.com>
2013-12-15 13:02:04 -0800

    Version définitive

---
+++ parser/src/TableSymbole.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.*;
+import java.lang.*;
+
+public class TableSymbole{
+	Map<String,Variable> variables;
+	
+	public TableSymbole() {
+		variables = new HashMap<String,Variable>();
+	}
+	
+	public void addVar( String name, Variable v ) {
+		variables.put(name,v);
+	}
+	
+	public Variable getVar( String var ) {
+		return variables.get( var );
+	}
+}
---
+++ parser/src/Variable.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.lang.*;
+
+public class Variable{
+	public String type;
+	public String value;
+	// The place of the variable, ie. the line where it appears the first time (where it's declared)
+	// so we can load it later in the code using the place
+	public Integer place;
+	
+	public Variable( String _type, String _value, Integer _place ) {
+		type = _type;
+		value = _value;
+		place = _place;
+	}
+
+}
